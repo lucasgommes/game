0971657ad7d69a6b1a1ccc9dbae537e1d93067ae
Lucas Gomes <lukasgds66@outlook.com>
2023-11-22 21:39:42 -0300

    novas funções

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from Game import *
+from Verify import *
+game_win = [11,15,9,1,3,23,8,2,25,19,4,12,5,16]
 dic = {'Jogo 1': [1, 2, 5, 6, 8, 9, 11, 12, 15, 16, 18, 20, 21, 23, 25],
 'Jogo 2': [1, 2, 4, 6, 8, 10, 11, 12, 14, 16, 17, 18, 21, 24, 25],
 'Jogo 3': [1, 2, 4, 5, 9, 10, 11, 12, 14, 15, 16, 17, 21, 23, 24],
@@ -8,7 +10,11 @@ dic = {'Jogo 1': [1, 2, 5, 6, 8, 9, 11, 12, 15, 16, 18, 20, 21, 23, 25],
 'Jogo 7': [2, 4, 5, 6, 9, 10, 12, 14, 15, 16, 18, 20, 23, 24, 25],
 'Jogo 8': [1, 2, 5, 6, 8, 9, 10, 11, 14, 15, 16, 17, 21, 23, 24],
 'Jogo 9': [1, 2, 4, 5, 6, 8, 9, 10, 11, 15, 16, 17, 18, 21, 24],
-'Jogo 10': [1, 2, 4, 6, 8, 9, 10, 12, 14, 16, 17, 18, 20, 23, 25],}
+'Jogo 10': [1, 2, 4, 6, 8, 9, 10, 12, 14, 16, 17, 18, 20, 23, 25],
+'Jogo 11': [1,2,3,5,6,8,12,13,15,19,20,21,22,25]}
 
-printDic(countRepeatedNumbersDic(dic))
-print(NoRepeat(dic))
+
+_res = quantidade_numeros_sorteados(dic, game_win)
+imprimir_lista_verde(sorted(game_win))
+imprimir_jogos_destacando(dic, game_win)
+render(_res)
