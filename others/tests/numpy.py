cce692291e005c678f113603925866d81ab69c6f
Lucas Gomes <lukasgds66@outlook.com>
2024-02-15 11:25:59 -0300

    Mega atualizado! aiin

---
+++ others/tests/numpy.py
@@ -0,0 +1,12 @@
+import random
+
+vetor_60 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+       31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+no_number = []
+
+for i in range(30):
+    num = random.choice(vetor_60)
+    vetor_60.remove(num)
+    no_number.append(num)
+
+print(sorted(no_number))
