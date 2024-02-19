08ad7a2f088fe5a2e7a71bb7c57a67c8ad7597a6
Lucas Gomes <lukasgds66@outlook.com>
2024-02-19 11:00:15 -0300

    meu mano, a mega vai dar bom demais

--- Mega.py
+++ Mega.py
@@ -10,13 +10,13 @@ vetor_60 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
        31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
        41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
        51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
-no_number = []
+no_number = [1, 2, 3, 4, 5, 7, 8, 9, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 25, 28, 29, 30, 31, 32, 33, 35, 36, 37, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 52, 53, 54, 56, 58, 59, 60]
     
 if no_number:
         for i in no_number:
             vetor_60.remove(i)
 else:  
-    for _ in range(24):
+    for _ in range(48):
             num = random.choice(vetor_60)
             vetor_60.remove(num)
             no_number.append(num)
@@ -33,10 +33,13 @@ def Main():
     for i, vetor in enumerate(vetores_de_6):
         print(f"{i + 1}: {sorted(vetor)},")
 
-# [1, 2, 7, 9, 11, 14, 15, 17, 19, 22, 27, 28, 29, 32, 33, 35, 36, 41, 43, 44, 46, 56, 57, 60]
+
 def verify():
-    game_win =[10,20,30,42,47,53]
-    d = {}
+    game_win =[10,17,26,34,43,51]
+    d = {1: [10, 17, 24, 27, 51, 55],
+         2: [6, 26, 34, 38, 43, 57],
+         3: [10, 34, 43, 51, 55, 57],
+         4: [6, 17, 24, 26, 27, 38],}
     _res = quantidade_numeros_sorteados(d, game_win)
     imprimir_lista_verde(sorted(game_win))
     imprimir_jogos_destacando(d, game_win)
